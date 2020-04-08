74a719814cd9e301ced7150f09c4be6d4f362492
tell723 <terumu723@google.com>
2020-04-08 14:39:27 +0900

    Initial Commit

---
+++ RubiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RubiApp
+//
+//  Created by 渡邊輝夢 on 2020/04/08.
+//  Copyright © 2020 Terumu Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
